204737242d04ed43a2ce16d8f2af6056c77b708a
AyoubMS <chahayb563@gmail.com>
2023-07-05 14:08:26 +0200

    add app

---
+++ InterfaceDemo/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceDemo
+{
+    // Interface is a contract
+    // Classes implement interfaces
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<IComputerController> controllers = new List<IComputerController>();
+
+            Keyboard keyboard = new Keyboard();
+            GameController gameController = new GameController();
+            BatteryPoweredGameController battery = new BatteryPoweredGameController();
+            BatteryPoweredKeyboard batteryKeyboard = new BatteryPoweredKeyboard();
+
+            controllers.Add(keyboard);
+            controllers.Add(gameController);
+            controllers.Add(battery);
+
+            foreach (IComputerController controller in controllers)
+            {
+                if (controller is GameController gc)
+                {
+
+                }
+
+                if (controller is IBatteryPowered powered)
+                {
+                    powered
+                }
+            }
+
+            List<IBatteryPowered> powereds = new List<IBatteryPowered>();
+
+            powereds.Add(battery);
+            powereds.Add(batteryKeyboard);
+
+            Console.ReadLine();
+        }
+    }
+
+    public interface IComputerController : IDisposable
+    {
+        void Connect();
+        void CurrentKeyPressed();
+    }
+
+    public class Keyboard : IComputerController
+    {
+        public void Connect()
+        {
+
+        }
+
+        public void CurrentKeyPressed()
+        {
+
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ConnectionType { get; set; }
+        public int BatteryLevel { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+
+    public interface IBatteryPowered
+    {
+        int BatteryLevel { get; set; }
+
+    }
+
+    public class BatteryPoweredKeyboard : Keyboard, IBatteryPowered
+    {
+        public int BatteryLevel { get; set; }
+
+    }
+
+    public class GameController : IComputerController, IDisposable
+    {
+        public void Connect()
+        {
+
+        }
+
+        public void CurrentKeyPressed()
+        {
+
+        }
+
+        public void Dispose()
+        {
+            // do whatever shutdown tasks needed
+        }
+
+    }
+
+    public class BatteryPoweredGameController : GameController, IBatteryPowered
+    {
+        public int BatteryLevel { get; set; }
+
+    }
+}
